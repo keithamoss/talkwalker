8fa21398176e96e9945d2784d3ceaf538b6ebdb4
keithamoss <keithamoss@gmail.com>
2020-04-19 17:33:17 +0800

    Updates

---
+++ precache-manifest.180d27cb01d593109e1a6879c6332ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1247985051cf771f2ea3b6ccb6e5f86c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005f8f55fb68d99dff8b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f6217ec566fa4ef3539b",
+    "url": "/static/js/2.a6d7bb30.chunk.js"
+  },
+  {
+    "revision": "d36ad493f88d33834380d547126a33eb",
+    "url": "/static/js/2.a6d7bb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005f8f55fb68d99dff8b",
+    "url": "/static/js/main.c83d3f23.chunk.js"
+  },
+  {
+    "revision": "959e21273555565fc929",
+    "url": "/static/js/runtime-main.5a6fca0f.js"
+  }
+]);
